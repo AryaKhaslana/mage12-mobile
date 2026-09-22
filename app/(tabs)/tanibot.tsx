1b0b4ad909a943f85f9463f1e6b38dab4a143fc3
Braja Khaslana <aryachakabrajawisesa@gmail.com>
2026-09-22 10:31:30 +0700

    fix(ui): remove paddingBottom gap in TaniBot input area

--- app/(tabs)/tanibot.tsx
+++ app/(tabs)/tanibot.tsx
@@ -372,7 +372,7 @@ const styles = StyleSheet.create({
   inputArea: {
     flexDirection: 'row',
     alignItems: 'flex-end',
-    paddingBottom: 36,
+    
     padding:16,
     backgroundColor: '#FFFFFF',
     borderTopWidth: 2,
